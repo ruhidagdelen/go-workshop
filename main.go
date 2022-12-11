db6b66c46227ffc8f2fea83d520c60b29b2cd73d
ruhi <ruhi@socradar.io>
2022-12-11 16:01:29 +0300

    structs

--- main.go
+++ main.go
@@ -6,10 +6,41 @@ func brainwash(saying *string) {
 	*saying = "Beep Boop."
 }
 
+type person struct {
+	name string
+	age  int
+}
+
+func newPerson(name string) *person {
+
+	p := person{name: name}
+	p.age = 42
+	return &p
+}
+
 func main() {
 	greeting := "Hello there!"
 
 	brainwash(&greeting)
 
 	fmt.Println("greeting is now:", greeting)
+
+	fmt.Println(person{"Bob", 20})
+
+	fmt.Println(person{name: "Alice", age: 30})
+
+	fmt.Println(person{name: "Fred"})
+
+	fmt.Println(&person{name: "Ann", age: 40})
+
+	fmt.Println(newPerson("Jon"))
+
+	s := person{name: "Sean", age: 50}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
 }
