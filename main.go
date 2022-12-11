0ea7bae6f40947bbb5c302a304b2eafcdad7501f
ruhi <ruhi@socradar.io>
2022-12-11 12:26:46 +0300

    a function declared

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
-import "time"
+import (
+	"fmt"
+	"math/rand"
+	"rsc.io/quote"
+	"time"
+)
+
+func rollDice() int {
+	var dice int
+	dice = rand.Intn(6)
+	return dice + 1
+}
+
+func rollDices(diceCount int) []int {
+	var diceResults []int
+	for i := 0; i < diceCount; i++ {
+		diceResults = append(diceResults, rollDice())
+	}
+	return diceResults
+}
 
 func main() {
 	fmt.Println("Hello World")
@@ -24,5 +41,15 @@ func main() {
 	//helloString = fmt.Sprintf("%s %s %s", quote.Hello(), fillerText, time.Now())
 
 	fmt.Println(helloString)
+	fmt.Println("---------------------")
+
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("A dice rolled: ", rollDice())
+
+	fmt.Printf("2 Dices Rolled: %v\n", rollDices(2))
+
+	fmt.Printf("3 Dices Rolled: %v\n", rollDices(3))
+
+	fmt.Printf("4 Dices Rolled: %v\n", rollDices(4))
 
 }
