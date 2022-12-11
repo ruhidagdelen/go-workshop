6811645055742ba54fb7ca01cc5d275b79cb473b
ruhi <ruhi@socradar.io>
2022-12-11 11:41:15 +0300

    imports shown

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
 import "fmt"
+import "rsc.io/quote"
+import "time"
 
 func main() {
 	fmt.Println("Hello World")
+
+	fmt.Println(quote.Go())
+
+	fmt.Println(quote.Hello())
+
+	fmt.Println(time.Now())
 }
