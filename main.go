3cc1d43d6155fbfeae6ebda2315a28d0309dfb00
ruhi <ruhi@socradar.io>
2022-12-13 19:24:19 +0300

    variadic functions example

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
 func main() {
-	fmt.Println("Hello World")
+
+	sum(1, 2)
+	sum(1, 2, 3)
+
+	nums := []int{1, 2, 3, 4}
+	sum(nums...)
 }
