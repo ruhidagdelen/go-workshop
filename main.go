db3eecf4de492cc97ce3cd17efd082243a7be105
ruhi <ruhi@socradar.io>
2022-12-17 14:46:59 +0300

    go declaring variables

--- main.go
+++ main.go
@@ -7,22 +7,32 @@ import "time"
 func main() {
 	fmt.Println("Hello World")
 
-	//fmt.Println(quote.Go())
+	//var helloString string
 	//
-	//fmt.Println(quote.Hello())
-	//
-	//fmt.Println(time.Now())
+	//helloString = fmt.Sprintf("%s %s", quote.Hello(), time.Now())
 
 	var helloString string
 
-	helloString = fmt.Sprintf("%s %s", quote.Hello(), time.Now())
+	fillerText := "The exact time is"
 
-	//var helloString string;
-	//
-	//fillerText := "The exact time is"
-	//
-	//helloString = fmt.Sprintf("%s %s %s", quote.Hello(), fillerText, time.Now())
+	helloString = fmt.Sprintf("%s %s %s", quote.Hello(), fillerText, time.Now())
 
 	fmt.Println(helloString)
 
+	theBestInteger := 61.0
+
+	//theBestInteger = 61.5
+
+	theSecondBest := 61.5
+
+	sumOfThem := theBestInteger + theSecondBest
+
+	fmt.Println(sumOfThem)
+
+	var a string
+	a = ""
+	_ = a
+
+	b := 1.2
+	_ = b
 }
