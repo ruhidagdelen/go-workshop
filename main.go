ef1e510173941e7d0ba9ffe4830bde7e77df52be
ruhi <ruhi@socradar.io>
2022-12-17 16:28:27 +0300

    data types

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"rsc.io/quote"
 	"strings"
-	"time"
 )
 
 func rollDice() int {
@@ -23,35 +21,6 @@ func rollDices(diceCount int) []int {
 }
 
 func main() {
-	fmt.Println("Hello World")
-
-	//fmt.Println(quote.Go())
-	//
-	//fmt.Println(quote.Hello())
-	//
-	//fmt.Println(time.Now())
-
-	var helloString string
-
-	helloString = fmt.Sprintf("%s %s", quote.Hello(), time.Now())
-
-	//var helloString string;
-	//
-	//fillerText := "The exact time is"
-	//
-	//helloString = fmt.Sprintf("%s %s %s", quote.Hello(), fillerText, time.Now())
-
-	fmt.Println(helloString)
-	fmt.Println("---------------------")
-
-	//rand.Seed(time.Now().UnixNano())
-	//fmt.Println("A dice rolled: ", rollDice())
-	//
-	//fmt.Printf("2 Dices Rolled: %v\n", rollDices(2))
-	//
-	//fmt.Printf("3 Dices Rolled: %v\n", rollDices(3))
-	//
-	//fmt.Printf("4 Dices Rolled: %v\n", rollDices(4))
 
 	var sampleInt int
 	var sampleString string
@@ -62,24 +31,24 @@ func main() {
 	fmt.Printf("default string '%v'\n", sampleString)
 	fmt.Printf("default float '%v'\n", sampleFloat)
 
-	// ARRAY
+	//ARRAY
 	var myArray [3]string
 
-	myArray = [3]string{"karadeniz", "teknik", "üniversitesi"}
+	myArray = [3]string{"karadeniz", "üniversitesi"}
 
 	//myArray = append(myArray, "trabzon")
 	for _, word := range myArray {
 		fmt.Printf("%v ", word)
 	}
-
-	// SLICE
-
+	//
+	//// SLICE
+	//
 	var mySlice []string
 	for _, word := range myArray {
 		mySlice = append(mySlice, word)
 	}
 	mySlice = append(mySlice, "trabzon")
-
+	//
 	concatenedText := strings.Join(mySlice, " ")
 	fmt.Println(concatenedText)
 
